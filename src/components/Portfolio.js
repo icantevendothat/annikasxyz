effe3cac1a066225e43aeb78618f8f6cb051783f
Annika Santhanam <aps659@nyu.edu>
2025-10-09 10:32:18 -0400

    checkout update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -110,7 +110,7 @@ const Portfolio = ({ onGridItemClick }) => {
     return (
         <div className="portfolio-container">
             <div class="tagline">
-                <a href="https://www.youtube.com/watch?v=xMYm2d9bmEA" style={{ color: '#FFFFFF' }} target="_blank" rel="noopener noreferrer">
+                <a href="https://slownet.work/" style={{ color: '#FFFFFF' }} target="_blank" rel="noopener noreferrer">
                     check this out
                 </a>
                 <p>art, design, technology, misbehavior, etc.</p>
